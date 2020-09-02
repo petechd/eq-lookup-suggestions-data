e48a650fe1174f9eb4e3048e9465886d01c32d76
Mark Price <pricem14pc@users.noreply.github.com>
2020-09-02 12:16:55 +0100

    Make source and data files language specific and update conversion script (#6)
    * Make source and data files language specific and update conversion script
    
    * Adding new data files
    
    * Updating README to align with latest language support requirements

--- README.md
+++ README.md
@@ -12,9 +12,13 @@ This repository contains versioned TextField suggestion data for eq-questionnair
 | religions.json | List of religions |
 
 
-Source data files are provided by the business as multiple column csv files. These can be manually added/updated in this repository at `./source-data`
+## Source csv files
 
-Separate source data files are provided for Northern Ireland at `./source-data/ni`. The non Northern Ireland source data files are located at `./source-data/gb` 
+Source data files are provided by the business as single column csv files. These can be manually added/updated in this repository at `./source-data`
+
+Separate source data files are provided for the Northern Ireland region at `./source-data/ni/en` and the non Northern Ireland (GB) region at `./source-data/gb`
+
+For the GB region, separate source data files are provided for the Welsh and English language versions of the suggestions lists at `./source-data/gb/cy` and `./source-data/gb/en` respectively.
 
 - source files to be provided by the business named for the above datasets
 
@@ -22,14 +26,14 @@ Separate source data files are provided for Northern Ireland at `./source-data/n
 
 - any values including commas should be double quoted
 
-- header row to contain comma separated language_codes (i.e `en-gb`, `cy`, `ga`, `eo`)
+- data rows to contain lookup terms (as to be presented in the lookup lists)
 
-- data rows to contain comma separated translated lookup values (as to be presented in the lookup lists)
+## Front-end json files
 
-- null values for a given language item can be provided by a non-spaced commas. This will result in that term not being presented as a suggestion for that language
-
-json files can be generated from the source csv files using `./scripts/convert_csv_to_json.py`
+json files can be generated from the source csv files using `./scripts/convert_csv_to_json.py`.
 
 - csv source file root directory `./source-data`
 
 - json output file root directory `./data`
+
+- two further levels of sub directories are expected corresponding to the `{region}` and `{language_code}` of the suggestions files respectively
--- scripts/convert_csv_to_json.py
+++ scripts/convert_csv_to_json.py
@@ -10,44 +10,48 @@ def generate_json_files():
     source_root = './source-data'
     output_root = './data'
 
+    for source_region_directory_name in os.listdir(source_root):
+        source_region_directory = os.path.join(source_root, source_region_directory_name)
 
-    for source_directory_name in os.listdir(source_root):
+        for language_directory_name in os.listdir(source_region_directory):
+            source_directory = os.path.join(source_region_directory, language_directory_name)
 
-        source_directory = os.path.join(source_root, source_directory_name)
+            for source_file_name in os.listdir(source_directory):
 
-        for source_file_name in os.listdir(source_directory):
+                source_file_location = os.path.join(source_directory, source_file_name)
 
-            source_file_location = os.path.join(source_directory, source_file_name)
+                if os.path.isfile(source_file_location):
 
-            if os.path.isfile(source_file_location):
+                    source_file = open(source_file_location, 'r')
 
-                source_file = open(source_file_location, 'r')
+                    output_file_name = source_file_name.replace('.csv', '.json')
+                    output_file_location = os.path.join(output_root,
+                                                        source_region_directory_name,
+                                                        language_directory_name,
+                                                        output_file_name)
 
-                output_file_name = source_file_name.replace('.csv', '.json')
-                output_file_location = os.path.join(output_root, source_directory_name, output_file_name)
+                    json_data = []
 
-                json_data = []
+                    with open(source_file_location, newline='') as csv_file:
+                        for row in csv.DictReader(csv_file, fieldnames=['term']):
+                            json_data.append(row.get('term'))
+                    try:
+                        with open(output_file_location, 'w', encoding='utf8') as f:
+                            json.dump(json_data, f, indent=4, ensure_ascii=False)
+                            f.write("\n")
+                    except FileNotFoundError:
+                        print(
+                            f"Output could not be written to {output_file_location}\nDoes the output folder"
+                            f"{output_root}/{source_region_directory_name}/{language_directory_name} exist?"
+                        )
+                        return
 
-                with open(source_file_location, newline='') as csv_file:
-                    for row in csv.DictReader(csv_file):
-                        # Append the data item as a json key-value if not an empty string
-                        json_data.append({k: v for k, v in row.items() if v})
-                try:
-                    with open(output_file_location, 'w', encoding='utf8') as f:
-                        json.dump(json_data, f, indent=3, ensure_ascii=False)
-                        f.write("\n")
-                except FileNotFoundError:
-                    print(
-                        f"Output could not be written to {output_file_location}\nDoes the output folder {output_root}/{source_directory_name} exist?"
-                    )
-                    return
+                    print(f"{source_file.name} >> {output_file_location}")
 
-                print(f"{source_file.name} >> {output_file_location}")
-
-                source_file.close()
-            else:
-                print(f"{source_file_location} is not a regular file")
+                    source_file.close()
+                else:
+                    print(f"{source_file_location} is not a regular file")
 
 
 if __name__ == "__main__":
-        generate_json_files()
+    generate_json_files()
